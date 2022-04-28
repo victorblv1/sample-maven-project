aec660f386f3543baf5c8bd6f9f38722224f40ec
Viktors Belovs <mayuss89@yahoo.com>
2022-04-28 12:33:55 +0200

    chanche test argument to true

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -15,7 +15,7 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        // assertTrue( true );
-        assertTrue( false );
+        assertTrue( true );
+        // assertTrue( false );
     }
 }
